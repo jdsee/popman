78fcb17c885b8296d0689243b7b10af69b8b143b
jdsee <joscha.seelig@.linked-planet.com>
2024-08-03 22:10:28 +0200

    Add fzf-prompt

--- popman.plugin.zsh
+++ popman.plugin.zsh
@@ -41,7 +41,7 @@ popman() {
     choice=$(echo "$cmds" | head -n 1)
   else
     # TODO: This should happen in the tmux popup instead of direcly in the buffer
-    choice=$(echo "$cmds" | fzf --layout=reverse)
+    choice=$(echo "$cmds" | fzf --layout=reverse --prompt="Select the tool you need help with: ")
   fi
   if [ "${TMUX}" ]; then
     tmux popup -EE -h 90% -w 90% man "$choice"
